4481c5a11685372b77e43549be816541c63f3ad3
Akretche Ales Samy <131418700+samAK02@users.noreply.github.com>
2025-12-19 22:37:52 +0100

    Update Formation.java

--- Formation.java
+++ Formation.java
@@ -5,17 +5,17 @@ import java.util.Set;
 public class Formation {
 	public String IdFormation;
 	public String intitule;
-	public String capacite;
+	public int capacite;
 	public Set<String> etudiantsInscrits;
 	
-	public Formation(String IdFormation, String intitule, String capacite) {
+	public Formation(String IdFormation, String intitule, int capacite) {
 		this.intitule = intitule;
 		this.capacite = capacite;
 		this.IdFormation = IdFormation;
 		this.etudiantsInscrits = new HashSet<>();
 	}
 	
-	public void ajouterEtudiant(String id) {
+	public void ajouterEtudiant(String IdEtudiant) throws FormationPleineException {
 		if (estPleine()) {
 			throw new FormationPleineException("Formation pleine");
 		}
@@ -23,14 +23,14 @@ public class Formation {
 		
 	}
 	
-	public void retirerEtudiant(String id) {
+	public void retirerEtudiant(String IdEtudiant) {
 		etudiantsInscrits.remove(IdEtudiant);
 		
 	}
 	
-	public boolean estPleine() {
-		return etudiantsInscrits.size() >= capacite;
-	}
+    public boolean estPleine() {
+        return etudiantsInscrits.size() >= capacite;
+    }
 	
 	public String toString() {
 		return "Id Formation: "+IdFormation+"Intitulé: "+intitule+"capacité: "+capacite+", Inscrits"+etudiantsInscrits.size();
