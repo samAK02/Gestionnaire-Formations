1803abcf44174e487a6ed055d6637102d683293b
Akretche Ales Samy <131418700+samAK02@users.noreply.github.com>
2025-12-25 16:46:52 +0100

    Delete Inscription.java

--- Inscription.java
+++
@@ -1,7 +0,0 @@
-package gestionPersonnes;
-
-public interface Inscription {
-	void inscrire(String idFormation) throws FormationIntrouvableException, FormationPleineException;
-	void desinscrire(String idFormation) throws FormationIntrouvableException;
-
-}
